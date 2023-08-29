030451392612bb61dee3301ff9839cb89b673711
gerzon-sz <gerzon.udang@talentamp.com>
2023-08-29 13:52:53 +0800

    fixed header and content width per view port

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,13 +1,22 @@
 import { Outlet } from 'react-router-dom';
 import ResponsiveAppBar from './ResponsiveAppBar';
-
+import * as React from 'react';
+import SideNav from './SideNav';
+import CssBaseline from '@mui/material/CssBaseline';
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
 const Layout = () => {
     return (
         <>
-            <ResponsiveAppBar />
-            <main className="App">
-                <Outlet />
-            </main>
+            <CssBaseline />
+            <Container maxWidth="lg" sx={{ width:"100vw" }}>
+                <ResponsiveAppBar />
+            </Container>
+            <Container sx={{ height:"100vh" }}>
+                <main className="App">
+                    <Outlet />
+                </main>
+            </Container>
         </>
     )
 }
---
+++ src/components/MainContent.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Container from '@mui/material/Container';
+
+const MainContent = () => {
+  return (
+    <Container>
+      {/* Your main content goes here */}
+      <h1>Main Content</h1>
+    </Container>
+  );
+};
+
+export default MainContent;
