86292272b497b93724684ccda8e0d737b33ca104
gerzon-sz <gerzon.udang@talentamp.com>
2023-08-30 16:23:36 +0800

    finalizing

--- src/features/posts/AddPostForm.jsx
+++ src/features/posts/AddPostForm.jsx
@@ -1,8 +1,7 @@
-import { useState } from "react";
-import { useNavigate } from "react-router-dom";
-import { useAddNewPostMutation } from "./postsSlice";
-import { useGetUsersQuery } from "../users/usersSlice";
-
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useAddNewPostMutation } from './postsSlice';
+import { useGetUsersQuery } from '../users/usersSlice';
 import styled from '@emotion/styled';
 import TextField from '@mui/material/TextField';
 import Select from '@mui/material/Select';
@@ -10,93 +9,99 @@ import InputLabel from '@mui/material/InputLabel';
 import FormControl from '@mui/material/FormControl';
 import Button from '@mui/material/Button';
 import MenuItem from '@mui/material/MenuItem';
+import { useForm, Controller } from 'react-hook-form';
+
 const StyledForm = styled.form`
-   display: flex;
-   row-gap: 1rem;
-`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+`;
+
 const AddPostForm = () => {
-    const [addNewPost, { isLoading }] = useAddNewPostMutation()
+  const [addNewPost, { isLoading }] = useAddNewPostMutation();
+  const navigate = useNavigate();
+  const { control, handleSubmit, formState } = useForm();
+  const { data: users, isSuccess } = useGetUsersQuery('getUsers');
 
-    const navigate = useNavigate()
+  const canSave = !formState.isSubmitting && isSuccess;
 
-    const [title, setTitle] = useState('')
-    const [content, setContent] = useState('')
-    const [userId, setUserId] = useState('')
-
-    const { data: users, isSuccess } = useGetUsersQuery('getUsers')
-
-    const onTitleChanged = e => setTitle(e.target.value)
-    const onContentChanged = e => setContent(e.target.value)
-    const onAuthorChanged = e => setUserId(e.target.value)
-
-
-    const canSave = [title, content, userId].every(Boolean) && !isLoading;
-
-    const onSavePostClicked = async () => {
-        if (canSave) {
-            try {
-                await addNewPost({ title, body: content, userId }).unwrap()
-
-                setTitle('')
-                setContent('')
-                setUserId('')
-                navigate('/')
-            } catch (err) {
-                console.error('Failed to save the post', err)
-            }
-        }
+  const onSubmit = async (data) => {
+    if (canSave) {
+      try {
+        await addNewPost(data).unwrap();
+        navigate('/');
+      } catch (err) {
+        console.error('Failed to save the post', err);
+      }
     }
+  };
 
-    let usersOptions
-    if (isSuccess) {
-        usersOptions = users.ids.map(id => (
-            <MenuItem key={id} value={id}>
-                {users.entities[id].name}
-            </MenuItem>
-        ))
-    }
-
-    return (
-        <section>
-        <h2>Add a New Post</h2>
-       
-            <StyledForm>
-            <TextField id="outlined-basic" label="Post Title" variant="outlined"
-             value={title}
-             onChange={onTitleChanged}
+  return (
+    <section>
+      <h2>Add a New Post</h2>
+      <StyledForm onSubmit={handleSubmit(onSubmit)}>
+        <Controller
+          name="title"
+          control={control}
+          defaultValue=""
+          render={({ field }) => (
+            <TextField
+              id="outlined-basic"
+              label="Post Title"
+              variant="outlined"
+              {...field}
             />
+          )}
+        />
+        <Controller
+          name="userId"
+          control={control}
+          defaultValue=""
+          render={({ field }) => (
             <FormControl fullWidth>
-                <InputLabel id="demo-simple-select-label">Author</InputLabel>
-                <Select
-                    id="demo-simple-select"
-                    labelId="demo-simple-select-label"
-                    label="Author"
-                    value={userId}
-                    onChange={onAuthorChanged}
-                    >
-                        {usersOptions}
-                
-                </Select>
+              <InputLabel id="demo-simple-select-label">Author</InputLabel>
+              <Select
+                id="demo-simple-select"
+                labelId="demo-simple-select-label"
+                label="Author"
+                {...field}
+              >
+                {isSuccess &&
+                  users.ids.map((id) => (
+                    <MenuItem key={id} value={id}>
+                      {users.entities[id].name}
+                    </MenuItem>
+                  ))}
+              </Select>
             </FormControl>
-
-            <TextField label="Post Content" variant="outlined"
-                id="postContent"
-                name="postContent"
-                value={content}
-                onChange={onContentChanged}
-                multiline
-                rows = {8}
+          )}
+        />
+        <Controller
+          name="content"
+          control={control}
+          defaultValue=""
+          render={({ field }) => (
+            <TextField
+              label="Post Content"
+              variant="outlined"
+              id="postContent"
+              name="postContent"
+              multiline
+              rows={8}
+              {...field}
             />
-            <Button 
-            variant="contained"
-            disabled={!canSave}
-            onClick={onSavePostClicked}
-
-            >Save Post</Button>
-         
-            </StyledForm>
-        
+          )}
+        />
+        <Button
+          type="submit"
+          variant="contained"
+          disabled={!canSave}
+        >
+          Save Post
+        </Button>
+      </StyledForm>
     </section>
-    )
-}
-export default AddPostForm
+  );
+};
+
+export default AddPostForm;
--- src/features/users/UsersList.jsx
+++ src/features/users/UsersList.jsx
@@ -1,8 +1,14 @@
+
 import { Link } from 'react-router-dom'
+import {useState, useEffect} from 'react'
 import { useGetUsersQuery } from './usersSlice'
+import { DataGrid } from '@mui/x-data-grid';
 
-const UsersList = () => {
-
+export default function UsersList() {
+    
+const [selectedRows, setSelectedRows] = useState([]);
+const [content, setContent] = useState(<p>Loading...</p>);
+let data
     const {
         data: users,
         isLoading,
@@ -11,29 +17,65 @@ const UsersList = () => {
         error
     } = useGetUsersQuery('getUsers')
 
-    let content;
+
     if (isLoading) {
-        content = <p>"Loading..."</p>;
+        
     } else if (isSuccess) {
-
-        const renderedUsers = users.ids.map(userId => (
-            <li key={userId}>
-                <Link to={`/user/${userId}`}>{users.entities[userId].name}</Link>
-            </li>
-        ))
-
-        content = (
-            <section>
-                <h2>Users</h2>
-
-                <ul>{renderedUsers}</ul>
-            </section>
-        )
+        data = {
+            columns:Object.keys(users.entities[1]).map((key) => ({
+                field: key,
+                headerName: key,
+                width: 150,
+            })),
+            rows: Object.values(users.entities)
+        }
     } else if (isError) {
-        content = <p>{error}</p>;
+        
     }
 
-    return content
+
+  useEffect(() => {
+      if (isSuccess) {
+        data = {
+            columns:Object.keys(users.entities[1]).map((key) => ({
+                field: key,
+                headerName: key,
+                width: 150,
+            })),
+            rows: Object.values(users.entities)
+        }
+                console.log(data)
+        console.log(users)
+     setContent(
+     
+      )
+      }
+    }, [isSuccess])
+useEffect(() => {
+       setContent(<p>Loading...</p>)
+      }, [isLoading])
+     return (
+        <>
+        {isSuccess && (<div style={{ height: 400, width: '100%' }}>
+        <DataGrid
+
+            rows={data.rows}
+            columns={data.columns}
+            pageSize={5}
+            rowsPerPageOptions={[5]}
+            checkboxSelection
+            onSelectionModelChange={(newSelection) => {
+                console.log(newSelection)
+                setSelectedRows(newSelection.selectionModel);
+            }}
+            selectionModel={selectedRows}
+        />
+    </div> )}
+
+            {isLoading && (<p>Loading...</p>)}
+    
+
+        </>
+    )
 }
 
-export default UsersList
